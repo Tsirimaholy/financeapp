4b864eca7039ec5ce165173792a023a5db213a57
Tsirimaholy <hei.tsirimaholy@gmail.com>
2025-05-15 17:16:56 +0300

    init

---
+++ financeapp/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from financeapp.users.models import User
+from financeapp.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
